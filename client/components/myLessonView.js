9c833afb90a8aaee876c97c99b106de2749f90a2
Isto <istobarton@gmail.com>
2015-08-07 14:51:24 -0500

    Beginning of myLessonsView.js

---
+++ client/components/myLessonView.js
@@ -0,0 +1,16 @@
+var LessonBox = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>
+          My Lessons
+        </h1>
+      </div>
+    )
+  }
+})
+
+React.render(
+  <LessonBox />,
+  document.getElementById('')
+  )
