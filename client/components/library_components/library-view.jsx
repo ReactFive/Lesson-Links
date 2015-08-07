b60d220c5e37166187b25b2887cf6fa059f268f3
Isto <istobarton@gmail.com>
2015-08-07 15:55:35 -0500

    Library View Routed In

---
+++ client/components/library_components/library-view.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+//**Declare additional variable necessary for view
+
+//var LibFilter = require('./lib-filter.jsx');
+
+//var LibFilterHeader = require('./lib-filter-header')
+
+//var LibLessonOptions = require('./lib-lesson-options.jsx');
+
+//var LibLessonPreview = require('./lib-lesson-preview.jsx');
+
+var LibraryView = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>
+          My Lessons
+        </h1>
+      </div>
+    );
+  }
+});
+
+module.exports = LibraryView;
