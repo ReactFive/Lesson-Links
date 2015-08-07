28b2e6b047b1810810553e6f283c3226f391e160
Abhi Gulati <abhigulati@gmail.com>
2015-08-06 19:39:05 -0500

    grabbed dependencies for videojs functionality

--- client/components/App.js
+++ client/components/App.js
@@ -3,6 +3,8 @@ var React = require('react');
 var RouteHandler = require('react-router').RouteHandler;
 var CommentSubmission = require('./comment-submission.jsx');
 var CommentList = require('./comment-list.jsx');
+var VideoPlayer = require('./video-player.jsx');
+
 //require('../stylesheets/modules/app.scss');
 
 var App = React.createClass({
@@ -10,7 +12,7 @@ var App = React.createClass({
     return {
       title: "our video",
       url: "https://www.youtube.com/watch?v=xjS6SftYQaQ",
-      comments: []
+      comments: [{text:"test comment", time:60}]
     };
   },
   render: function() {
