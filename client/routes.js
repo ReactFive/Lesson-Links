3a3c09b16936b061a0acae733db282ab41e637a4
Abhi Gulati <abhigulati@gmail.com>
2015-08-07 11:19:19 -0500

    added route to lesson-view

--- client/routes.js
+++ client/routes.js
@@ -4,10 +4,12 @@ var Route = require('react-router').Route;
 
 var App = require('./components/App');
 var Home = require('./components/home');
+var LessonView = require('./components/lesson-view.jsx')
 
 var routes = (
     <Route handler={App}>
       <Route path='/' handler={Home} />
+      <Route path='/lesson' handler={LessonView} />
     </Route>
 );
 
