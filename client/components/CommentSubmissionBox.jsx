37ca2973bd86e40f607a7416731aa7e682961ee8
ecastano <esteban.rotary@gmail.com>
2015-08-08 15:23:15 -0500

    Added toggle feature for comment form

--- client/components/CommentSubmissionBox.jsx
+++ client/components/CommentSubmissionBox.jsx
@@ -3,25 +3,36 @@ var React = require('react');
 var CommentSubmissionBox = React.createClass({
   getInitialState: function(){
     return {
-      text: ''
+      text: '',
+      showCommentForm: false
     }
   },
   onChange: function(e) {
     this.setState({text: e.target.value});
   },
+  onToggleCommentForm: function(){
+    this.setState({ showCommentForm: !this.state.showCommentForm });
+  },
   handleSubmit: function(e) {
     e.preventDefault();
     this.props.submit(this.state.text);
+    this.onToggleCommentForm();
   },
   render: function() {
     return (
       <div id="comment-creation-box" className="panel panel-default">
         <div className="panel panel-body">
-          <button href="/" className="btn btn-primary" role="button">Make a comment</button>
-          <form onSubmit={this.handleSubmit}>
-            <textarea className="form-control" onChange={this.onChange} value={this.state.text}/>
-            <button className="btn btn-primary">submit</button>
-          </form>
+
+          <input type="submit" value="Question? Comment?" className="btn btn-primary" onClick={this.onToggleCommentForm} />
+          
+          { this.state.showCommentForm ? 
+            <form onSubmit={this.handleSubmit}>
+              <textarea className="form-control" onChange={this.onChange} value={this.state.text}/>
+              <input type="submit" value="submit" className="btn btn-primary" onClick={this.handleSubmit}/ >
+            </form>
+          : null }
+
+
 
         </div>
       </div>
