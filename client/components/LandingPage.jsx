0093c0832ed90f27c00e47e1b5af23ea7d25b391
Isto <istobarton@gmail.com>
2015-08-10 15:01:28 -0500

    Add sass pages for Library and landing

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -1,13 +1,17 @@
 var React = require('react');
 var Link = require('react-router').Link;
 var Nav = require('./header-footer/nav.jsx');
+
+require('../../sass/LandingPage.scss');
+
 var Home = module.exports = React.createClass({
 
   render: function () {
     return (
         <div>
           <div className='container'>
-            <h3 className='text-center'>This will be home.</h3>
+            <h1 className='text-center'>Lesson Links</h1>
+            <h3 className='text-center'>Annotated Educational Videos.</h3>
           </div>
         </div>
     )
