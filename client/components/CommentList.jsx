8e607a82d2c39eb3ccf8d364bbab8ce6abbc7b3f
ecastano <esteban.rotary@gmail.com>
2015-08-10 10:16:33 -0500

    Improved UI for comments/replies

--- client/components/CommentList.jsx
+++ client/components/CommentList.jsx
@@ -7,9 +7,9 @@ var CommentList = React.createClass({
     var comments = this.props.comments.map(function(comment, index) {
       return <li className="comment-box" key={index}> <Comment comment={comment} submitReply={that.props.submitReply}/> </li>
     });
-    return ( 
-      <div id="comment-list-container">
-        <ul id="comment-list">
+    return (
+      <div id="comment-list-container" className="panel panel-default">
+        <ul id="comment-list" className="panel panel-body">
           {comments}
         </ul>
       </div>
