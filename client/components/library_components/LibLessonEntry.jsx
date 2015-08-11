aa67525291464dc5948dbe00e251181fb6bc6081
Colin Wiley <colinwwiley@gmail.com>
2015-08-11 12:30:21 -0500

    Added MongoStore for sessions

--- client/components/library_components/LibLessonEntry.jsx
+++ client/components/library_components/LibLessonEntry.jsx
@@ -8,6 +8,13 @@ var LibLessonEntry = React.createClass({
     var lessons = this.props.lessons.map(function(lesson, index){
       return <span className="lib-lesson-entry" key={index}>
       
+          <ul>
+            <li>Title: {lesson.title}</li>
+            <li>
+              url: {lesson.url} 
+            </li>
+            <li>Author: {lesson.teacher.name}</li>
+          </ul> 
           <p>
             <p className="lib-less-title">{lesson.title}</p>
             <p className="lib-less-author">By: {lesson.teacher.name}</p>
