f39fcb5d12ef802a1cd23b98d82041e6339b8b1d
ecastano <esteban.rotary@gmail.com>
2015-08-11 09:24:05 -0500

    rebased with master

--- client/components/header-footer/footer.jsx
+++ client/components/header-footer/footer.jsx
@@ -2,30 +2,10 @@ var React = require('react');
 
 module.exports = React.createClass({
   render: function() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-   return ( <footer className="navbar navbar-default footer-main">
-          <small>Copyright Team Acid Reflux.</small>
-      </footer>
-=======
-<<<<<<< HEAD
-   return ( <footer className="navbar navbar-default">
-=======
-
    return ( 
-    <footer className="navbar navbar-default">
->>>>>>> Compile sass files into one file before re-rendering
-      <div className="container">
-        <div className="row">
-          <small>Copyright Team Acid Reflux.</small>
-        </div>
-      </div>
-    </footer>
-<<<<<<< HEAD
->>>>>>> Added submit reply to store and improved UI
-   )
-=======
+      <footer className="navbar navbar-default footer-main">
+        <small>Copyright Team Acid Reflux.</small>
+      </footer>
     )
->>>>>>> Compile sass files into one file before re-rendering
   }
 })
