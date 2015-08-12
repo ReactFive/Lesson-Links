eefe47572bf762f9c14bd1458ac15505646f8f40
Richard Van Camp <richard.vancamp@tut.fi>
2015-08-12 10:33:35 -0500

    setup page

---
+++ client/components/exerciseSetup/exerciseSetup.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var Reflux = require('reflux');
+var Router = require('react-router');
+var Navigation = Router.Navigation;
+var Link = Router.Link;
+var AuthStore = require('../../stores/AuthStore');
+var Actions = require('../../actions');
+var Select = require('react-select');
+
+var exerciseSetup = React.createClass({
+
+
+
+
+});
+
+module.exports = exerciseSetup;
