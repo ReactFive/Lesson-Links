d9dfee3a9dbe7e5918a8e4a55cf13d9e87c48186
Isto <istobarton@gmail.com>
2015-08-12 12:04:59 -0500

    Add basic structure to prepare for Reflux cycle

--- client/components/add_lesson_view/AddVideoBox.jsx
+++ client/components/add_lesson_view/AddVideoBox.jsx
@@ -4,18 +4,65 @@ var AddLessonStore = require('../../stores/AddLessonStore.js');
 
 var AddVideoBox = React.createClass({
 
+
+    getInitialState: function(){
+      return {
+        lessonTitle: '',
+        lessonURL: '',
+        lessonLink: ''      
+      }
+    },
+
+    onInputChange: function(e){
+      this.setState({lessonTitle: React.findDOMNode(this.refs.lessonTitle).value})
+      this.setState({lessonURL: React.findDOMNode(this.refs.lessonURL).value})
+      this.setState({lessonLink: React.findDOMNode(this.refs.lessonLink).value})
+    },
+
+    handleSubmit: function(e){
+      e.preventDefault();
+      debugger;
+    },
+    
   render: function() {
     return (
       <div>
         <div id="addvideo-box" >
-          <h1>
-            <input type="text" placeholder="What is the name of your new Lesson?" className="add-lesson-info"></input>
-          </h1>
-          <input type="url" className="add-lesson-info"placeholder="Input your video URL here. We currently only support YouTube videos." name="newVideoUrl"></input>
+          <form className="lessonInputFields">
+            <input 
+              className="add-lesson-info"
+              type="text" 
+              placeholder="What is the name of your new Lesson?"
+              ref="lessonTitle" 
+              value={this.state.lessonTitle}
+              onChange={this.onInputChange}></input>
+            <input 
+              className="add-lesson-info"
+              type="url" 
+              placeholder="Input your video URL here. We currently only support YouTube videos." 
+              name="newVideoUrl" ref="lessonURL" 
+              value={this.state.lessonURL}
+              onChange={this.onInputChange}></input>
+            <h1>www.lesson-links.com/api/lesson/</h1>
+            <input 
+              type="text" 
+              placeholder="Customize your Lesson Link." 
+              name="newVideoUrl" 
+              ref="lessonLink" 
+              value={this.state.lessonLink}
+              onChange={this.onInputChange}></input> 
+            <button
+              type="submit" 
+              className="btn btn-primary" 
+              onClick={this.handleSubmit}>Save & Continue</button>
+          </form>
         </div>
-        <div className="addless-button-container">
-          <button className="add-lesson-button">Save & Edit</button>
-          <button className="add-lesson-button">Reset Fields</button>
+
+        <div>
+          <h1> Results </h1>
+          {this.state.lessonTitle}
+          {this.state.lessonURL}
+          {this.state.lessonLink}
         </div>
       </div>
     );
--- client/stores/AddLessonStore.js
+++ client/stores/AddLessonStore.js
@@ -1,1 +1,9 @@
-//add lesson store goes
+var Reflux = require('reflux');
+var Api = require('../utils/api');
+var Actions = require('../actions');
+var _ = require('lodash');
+
+module.exports = Reflux.createStore({
+  listenables: [Actions],
+  
+})
