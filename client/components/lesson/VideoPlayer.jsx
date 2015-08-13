fc26e8bcef940702705029f6a32b94aa1e52f6a9
Abhi Gulati <abhigulati@gmail.com>
2015-08-13 11:58:47 -0500

    fixed some styles on video player to keep it from getting pushed down

--- client/components/lesson/VideoPlayer.jsx
+++ client/components/lesson/VideoPlayer.jsx
@@ -83,10 +83,10 @@ var VideoPlayer = React.createClass({
       this.state.lesson && this.state.lesson.video_url ?
         <div className="row">
           <div className="col-md-10 col-md-offset-1">
-            <div className="embed-responsive embed-responsive-16by9">
+            <div className="">
               <video id='attachmentVideo'
                 className='video-js vjs-default-skin'
-                width='640' height='390'
+                width='700' height='394'
                 controls preload='auto'
                 data-setup={'{ "techOrder": ["youtube"], "src": "' + this.state.lesson.video_url + '" }'}>
               </video>
