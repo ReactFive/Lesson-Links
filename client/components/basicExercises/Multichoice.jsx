8cd6e9c9d0bef0c273f3ecaa9fff97296ce237ed
Richard Van Camp <richard.vancamp@tut.fi>
2015-08-15 15:08:33 -0500

    trueFalse question working

--- client/components/basicExercises/Multichoice.jsx
+++ client/components/basicExercises/Multichoice.jsx
@@ -115,7 +115,7 @@ var Multichoice = React.createClass({
               <div className="modal-dialog">
                 <div className="modal-content">
                   <div className="modal-header">
-                    <h3><span className="label label-warning" id="qid">{i}</span>{question}</h3>
+                    <h3><span className="label label-default" id="qid"><i className="fa fa-question-circle fa-lg"></i></span>{question}</h3>
                   </div>
                   {/*end header*/}
                   <div className="modal-body">
