13f7a9b26a60099a795d53874c265bac239d2438
Esteban Castaño <esteban.rotary@gmail.com>
2015-08-17 19:02:14 -0500

    Update nav.jsx
    Changed fixed to static

--- client/components/header-footer/nav.jsx
+++ client/components/header-footer/nav.jsx
@@ -18,7 +18,7 @@ var nav = React.createClass({
   },
 
   render: function() {
-    return ( <nav className="navbar navbar-default navbar-fixed-top">
+    return ( <nav className="navbar navbar-default navbar-static-top">
       <div className="container">
         <div className="navbar-header">
           <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
