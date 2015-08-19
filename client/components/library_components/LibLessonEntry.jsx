82e5878e127f43c7133fd00a6fa652a311d7b8a7
Isto <istobarton@gmail.com>
2015-08-19 15:31:22 -0500

    Adjusted Library for smaller entries

--- client/components/library_components/LibLessonEntry.jsx
+++ client/components/library_components/LibLessonEntry.jsx
@@ -58,24 +58,24 @@ var LibLessonEntry = React.createClass({
     var imgUrl = 'http://img.youtube.com/vi/' + video_id + '/mqdefault.jpg'
 
     return (
-      <div className="lib-lesson-entry">
-        <img className="media pull-left videoSnippet" src={imgUrl} />
-        <ul className="lib-lesson-info list-unstyled">
-          <li className="lib-less-title">
-            <button className ="titleAnchor" onClick={lesson.publish ? this.gotoLesson : this.gotoConfigure}>
-              {lesson.title || 'title not found'}
-            </button>
-          </li>
-          <li className="lib-less-author">
-            Author: {lesson.teacher.name || 'anonymous'}
-          </li>
-          <li className="lib-lesson-stats">
-            Comments: {commentCount}    Exercises: {lesson.exercises.length}
-          </li>
-          {publishDisplay}
-        </ul>
-        <span className="fa fa-trash-o pull-right" onClick={this.deleteLesson}></span>
-      </div>
+      <li className="lib-lesson-entry" onClick={lesson.publish ? this.gotoLesson : this.gotoConfigure}>
+          <img className="media pull-left videoSnippet" src={imgUrl} />
+          <ul className="lib-lesson-info list-unstyled">
+            <li className="lib-less-title">
+              <a className ="titleAnchor">
+                {lesson.title || 'title not found'}
+              </a>
+            </li>
+            <li className="lib-less-author">
+              Author: {lesson.teacher.name || 'anonymous'}
+            </li>
+            <li className="lib-lesson-stats">
+              Comments: {commentCount}    Exercises: {lesson.exercises.length}
+            </li>
+            {publishDisplay}
+          </ul>
+          <span className="fa fa-trash-o pull-right" id="trashcan" onClick={this.deleteLesson}></span>
+      </li>
     );
   }
 
