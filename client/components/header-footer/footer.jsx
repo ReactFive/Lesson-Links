55cb7c69541bbe489e556160793f21cf46b7d3ee
ecastano <esteban.rotary@gmail.com>
2015-08-19 14:10:47 -0500

    Added footer class so footer is now sticky on bottom of pages

--- client/components/header-footer/footer.jsx
+++ client/components/header-footer/footer.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 module.exports = React.createClass({
   render: function() {
    return (
-       <div role="navigation">
+       <div role="navigation" className="footer">
         <div className="container">
           <div className="navbar-text pull-left">
             <p>© 2015 Team React 5</p>
