83a4675ca2866a57c8f99bfe0e33b54a369ff147
Richard Van Camp <richard.vancamp@tut.fi>
2015-08-21 14:30:19 -0500

    protected api endpoints

--- server/auth.js
+++ server/auth.js
@@ -1,7 +1,6 @@
 
 exports.requiresApiLogin = function (request, response, next) {
   if(!request.isAuthenticated()) {
-    console.log(!!request.isAuthenticated());
     response.status(403);
     response.end();
   } else {
