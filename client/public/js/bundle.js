8dadcd9db28dff1f31212a71662afe7947450d9d
Richard Van Camp <richard.vancamp@tut.fi>
2015-08-22 16:42:58 -0500

    client added

--- client/public/js/bundle.js
+++ client/public/js/bundle.js
@@ -4837,7 +4837,7 @@ module.exports = Reflux.createStore({
 
 var React = require('react');
 var axios = require('axios');
-var rootUrl = 'https://lesson-links.herokuapp.com/';
+var rootUrl = 'https://lesson-links.herokuapp.com';
 
 module.exports.login = function (email, password) {
   var url = rootUrl + '/api/login';
