0062d9e73210448f03fb05815b4839a8076c8a97
Isto <istobarton@gmail.com>
2015-08-31 21:32:45 -0500

    Adjusted landing img, team img, reduced all for loadtime

--- client/components/lesson/LoginOverlay.jsx
+++ client/components/lesson/LoginOverlay.jsx
@@ -8,6 +8,7 @@ var LoginOverlay
       <div className="login-overlay">
         <div className="arrow-box"> 
           <p className="arrow-box-message">Login or Signup to view this lesson!</p>
+        	{/*<img className="arrow-box-message" src="../../public/assets/Login.png" />*/}
         </div>
       </div>
     );
