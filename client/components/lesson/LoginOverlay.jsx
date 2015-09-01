90435249bb0a9ee7f911e11e4a46964607003418
Richard Van Camp <richard.vancamp@tut.fi>
2015-09-01 11:04:10 -0500

    changed loginPointer

--- client/components/lesson/LoginOverlay.jsx
+++ client/components/lesson/LoginOverlay.jsx
@@ -4,11 +4,24 @@ var LoginOverlay
  = React.createClass({
 
   render: function() {
+    var iconstyles = {
+      fontSize: '30px',
+      color: '#226395'
+    };
+    var modalstyles = {
+      maxHeight: '40px'
+    };
     return (
-      <div className="login-overlay">
-        <div className="arrow-box"> 
-          <p className="arrow-box-message">Login or Signup to view this lesson!</p>
-        	{/*<img className="arrow-box-message" src="../../public/assets/Login.png" />*/}
+        <div className="container-fluid col-md-offset-8 animated fadeInDown">
+          <div className="modal-dialog modal-sm">
+            <div styles={modalstyles} className="modal-content">
+              <div className="modal-body" id="special-modal">
+                <div className="col-offset-xs-1">
+                    <div className="pull-right"><span style={iconstyles} className="glyphicon glyphicon-arrow-up"></span></div>
+                    <h5>Please login or register in order to view your lesson</h5>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
